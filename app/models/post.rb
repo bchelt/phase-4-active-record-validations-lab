0f9b33f83c4d3f6be159e003d014c666547ca101
Brandon Helt <brandon.helt@yahoo.com>
2022-08-28 16:12:58 -0700

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_must_contain
+
+    def title_must_contain
+        unless title =~ /(Won't\sBelieve)|(Secret)|(Top\s\d)|(Guess)/
+            errors.add(:title)
+        end
+    end
 end
